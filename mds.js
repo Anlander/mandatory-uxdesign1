ce712fe8f85bb6286f3f891250feeeea5bdc88ed
Philip <anlanderp@gmail.com>
2019-01-30 22:14:03 +0100

    almost done

--- mds.js
+++ mds.js
@@ -1,16 +1,19 @@
 let textfield = document.querySelector('.mds-text-field__input');
 let label = document.querySelector('.mds-text-field__label');
 let underline = document.querySelector('.mds-text-field__underline');
+let placeholder = document.querySelector('placeholder');
 
 textfield.addEventListener('focus', () =>{
   label.classList.add('active');
   underline.classList.add('active');
+  placeholder.classList.add('active');
 })
 
 textfield.addEventListener('blur', () =>{
   if(textfield.value == ""){
     label.classList.remove('active');
     underline.classList.remove('active');
+    placeholder.classList.remove('active');
     console.log(underline)
   }
 })
