32c374cd29aa35c10cdf9b6934f91b1882655613
Lova Linfeldt <lova@Lovas-MacBook-Pro.local>
2019-01-31 17:30:13 +0100

    soon done

--- mds.js
+++ mds.js
@@ -1,19 +1,35 @@
 let textfield = document.querySelector('.mds-text-field__input');
 let label = document.querySelector('.mds-text-field__label');
 let underline = document.querySelector('.mds-text-field__underline');
-let placeholder = document.querySelector('placeholder');
+
 
 textfield.addEventListener('focus', () =>{
-  label.classList.add('active');
-  underline.classList.add('active');
-  placeholder.classList.add('active');
+    label.classList.add('active');
+    underline.classList.add('active')
+
+
 })
 
 textfield.addEventListener('blur', () =>{
   if(textfield.value == ""){
     label.classList.remove('active');
     underline.classList.remove('active');
-    placeholder.classList.remove('active');
     console.log(underline)
+    console.log(label);
+
   }
 })
+
+let buttons = document.querySelectorAll('button');
+
+
+function disable(){
+  for(let all of buttons){
+    let disall = document.querySelector('input, checkbox');
+    all.addEventListener('click',()=>{
+      disall.setAttribute("disabled" = true)
+      console.log(disall);
+    })
+  }
+
+}
